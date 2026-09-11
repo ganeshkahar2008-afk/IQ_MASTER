2c97636396573a1315075268202e6142e5fe8252
ganeshkahar2008-afk <ganeshkahar2008@gmail.com>
2026-09-11 13:05:47 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,24 @@
-const CACHE = "iq-master-v5";
+const CACHE = "iq-master-v6";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./sw.js",
-  "./icon.svg"
+  "./sw.js"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    )
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
